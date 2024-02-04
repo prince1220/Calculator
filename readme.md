248b95242a1b1585258a6d1eb28772bfb2246f12
prince1220 <frontdeskdesigns@gamil.com>
2024-02-04 16:45:41 -0500

    Implemented plus operations

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Calculator project
+
+## Technologies:
+ - HTML - CSS - JavaScript
+ 
+
+ """ To list """
+ - Complete the read me
+ - Complete the style.css
---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+plus.addEventListener("click",()=>{   ///invoke click event
+
+    let val1 = num1.value * 1
+    let val2 = num2.value * 1
+
+    result.innerHTML = val1 + val2
+
+
+})
+
